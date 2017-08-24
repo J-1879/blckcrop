5b934010e349d83eb4e5d50948414505f3b90bda
J-1879 <juan.mf789@gmail.com>
2017-08-23 22:32:27 -0500

    launch
    epic

---
+++ config-example.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'XXXXXXXXXXXXXXX',
+  consumer_secret:      'XXXXXXXXXXXXXXX',
+  access_token:         'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  access_token_secret:  'XXXXXXXXXXXXXXX'
+}
+
+module.exports = config;
